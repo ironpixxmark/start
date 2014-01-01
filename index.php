c453e6f27d4dc138f3df6793eecb2eb7b103891b
Faysal haque <iron@pixxmark.com>
2014-01-01 23:39:06 +0600

    clean index page

--- index.php
+++ index.php
@@ -46,72 +46,7 @@ $result = mysql_query($get);
 				</footer> 
 
 				<section> 
-					<!-- user --> 
-					<!-- <div class="bg-success nav-user hidden-xs pos-rlt"> 
-						<div class="nav-avatar pos-rlt"> 
-							<a href="#" class="thumb-sm avatar animated rollIn" data-toggle="dropdown"> 
-								<img src="images/avatar.jpg" alt="" class=""> 
-								<span class="caret caret-white"></span> 
-							</a>
-
-							<ul class="dropdown-menu m-t-sm animated fadeInLeft"> 
-								<span class="arrow top"></span> 
-								<li> <a href="#">Settings</a> </li> 
-								<li> <a href="profile.html">Profile</a> </li> 
-								<li> <a href="#"> <span class="badge bg-danger pull-right">3</span> Notifications </a> </li> 
-								<li class="divider"></li> 
-								<li> <a href="docs.html">Help</a> </li> 
-								<li> <a href="signin.html">Logout</a> </li> 
-							</ul> 
-
-							<div class="visible-xs m-t m-b"> 
-								<a href="#" class="h3">John.Smith</a> 
-								<p><i class="icon-map-marker"></i> London, UK</p> 
-							</div> 
-						</div> 
-
-						<div class="nav-msg"> 
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown"> 
-								<b class="badge badge-white count-n">2</b> 
-							</a> 
-
-							<section class="dropdown-menu m-l-sm pull-left animated fadeInRight"> 
-								
-								<div class="arrow left"></div> 
-								
-								<section class="panel bg-white"> 
-									
-									<header class="panel-heading"> 
-										<strong>You have <span class="count-n">2</span> notifications</strong> 
-									</header> 
-
-									<div class="list-group"> 
-										<a href="#" class="media list-group-item"> 
-											<span class="pull-left thumb-sm"> 
-												<img src="images/avatar.jpg" alt="John said" class="img-circle"> 
-											</span> 
-											<span class="media-body block m-b-none"> 
-												Use awesome animate.css<br> <small class="text-muted">28 Aug 13</small> 
-											</span> 
-										</a> 
-
-										<a href="#" class="media list-group-item"> 
-											<span class="media-body block m-b-none"> 
-												1.0 initial released<br> <small class="text-muted">27 Aug 13</small> 
-											</span> 
-										</a> 
-									</div> 
-
-									<footer class="panel-footer text-sm"> 
-										<a href="#" class="pull-right">
-											<i class="icon-cog"></i>
-										</a> 
-										<a href="#">See all the notifications</a> 
-									</footer> 
-								</section> 
-							</section> 
-						</div> 
-					</div> --> <!-- / user --> 
+					
 
 					<!-- nav --> 
 					<nav class="nav-primary hidden-xs"> 
